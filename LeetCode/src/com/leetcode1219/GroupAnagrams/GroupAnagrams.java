885aad8737615ba9df839148d1819c3f42d32426
Chengjingwen Tang <ctang8@stevens.edu>
2017-12-19 22:51:00 -0500

    add one solution

--- LeetCode/src/com/leetcode1219/GroupAnagrams/GroupAnagrams.java
+++ LeetCode/src/com/leetcode1219/GroupAnagrams/GroupAnagrams.java
@@ -41,4 +41,33 @@ public class GroupAnagrams {
 		}
 		return res;
 	}
+
+	// time : O(M*N) space: O(n)
+	public List<List<String>> groupAnagrams2(String[] strs) {
+		HashMap<String, List<String>> map = new HashMap<>();
+
+		for (String str : strs) {
+			int[] count = new int[26];
+			for (Character ch : str.toCharArray()) {
+				count[ch - 'a']++;
+			}
+			String s = "";
+			for (int i = 0; i < count.length; i++) {
+				if (count[i] != 0) {
+					s = s + String.valueOf(count[i]) + String.valueOf((char) i + 'a');
+				}
+			}
+			if (map.containsKey(s)) {
+				List<String> list = map.get(s);
+				list.add(str);
+			} else {
+				List<String> list = new ArrayList<>();
+				list.add(str);
+				map.put(s, list);
+			}
+
+		}
+		return new ArrayList<>(map.values());
+	}
+
 }
