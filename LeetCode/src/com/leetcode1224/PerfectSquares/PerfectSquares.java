d40c2a1ed4777e54b6af605e34c7fb82943ac6be
Chengjingwen Tang <ctang8@stevens.edu>
2017-12-24 23:14:11 -0500

    leetcode279. Perfect Squares

---
+++ LeetCode/src/com/leetcode1224/PerfectSquares/PerfectSquares.java
@@ -0,0 +1,27 @@
+package com.leetcode1222.PerfectSquares;
+
+import java.util.Arrays;
+
+/**
+ * Given a positive integer n, find the least number of perfect square numbers
+ * (for example, 1, 4, 9, 16, ...) which sum to n.
+ * 
+ * For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13,
+ * return 2 because 13 = 4 + 9.
+ * 
+ * @author tangc
+ *time:O(n*sqrt(n))  spqce:O(n)
+ */
+public class PerfectSquares {
+	public int numSquares(int n) {
+		int[] res = new int[n + 1];
+		Arrays.fill(res, Integer.MAX_VALUE);
+		res[0] = 0;
+		for (int i = 0; i <= n; i++) {
+			for (int j = 1; j * j <= i; j++) {
+				res[i] = Math.min(res[i], res[i - j * j] + 1);
+			}
+		}
+		return res[n];
+	}
+}
