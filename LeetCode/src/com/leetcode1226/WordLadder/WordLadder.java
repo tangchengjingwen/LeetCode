cd3ee810a6043e1ffbbb44458fa43468de2ff6ea
Chengjingwen Tang <ctang8@stevens.edu>
2017-12-26 16:01:32 -0500

    leetcode127. Word Ladder

--- LeetCode/src/com/leetcode1226/WordLadder/WordLadder.java
+++ LeetCode/src/com/leetcode1226/WordLadder/WordLadder.java
@@ -1,6 +1,10 @@
 package com.leetcode1226.WordLadder;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Given two words (beginWord and endWord), and a dictionary's word list, find
@@ -20,10 +24,40 @@ import java.util.List;
  * endWord are non-empty and are not the same.
  * 
  * @author tangc
- *
+ *time:O(n) space:O(n)
  */
 public class WordLadder {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        
-    }
+	public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+		HashSet<String> set = new HashSet<>(wordList);
+		if (set.contains(beginWord)) {
+			set.remove(beginWord);
+		}
+
+		Queue<String> queue = new LinkedList<>();
+		HashMap<String, Integer> map = new HashMap<>();
+		map.put(beginWord, 1);
+		queue.offer(beginWord);
+		while (!queue.isEmpty()) {
+			String word = queue.poll();
+			int curLevel = map.get(word);
+			for (int i = 0; i < word.length(); i++) {
+				char[] wordUnit = word.toCharArray();
+				// 每次改变一个字符然后判断是否在set中
+				for (char j = 'a'; j <= 'z'; j++) {
+					wordUnit[i] = j;
+					String tmp = new String(wordUnit);
+					if (set.contains(tmp)) {
+						if (tmp.equals(endWord)) {
+							return curLevel + 1;
+						} else {
+							map.put(tmp, curLevel + 1);
+							queue.offer(tmp);
+							set.remove(tmp);
+						}
+					}
+				}
+			}
+		}
+		return 0;
+	}
 }
